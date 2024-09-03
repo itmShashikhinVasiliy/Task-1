04003cfe0ad4f7098a400f5122083144735703c6
Shashikhin <shashihinv@gmail.com>
2024-09-03 15:03:15 +0300

    Решенная задача по описанию

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Ivan", "Sh", (byte) 1);
+        userService.saveUser("Vasya", "Hj", (byte) 26);
+        userService.saveUser("Tima", "Jk", (byte) 26);
+        userService.saveUser("Sasha", "Kl", (byte) 39);
+
+//        userService.removeUserById(3);
+
+        userService.getAllUsers().forEach(System.out::println);
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
