186d6260a94110dd12ce898fb74485b7c60a5167
Shashikhin <shashihinv@gmail.com>
2024-09-05 00:13:14 +0300

    Решённая задача 1.1.4 (Task1)

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Properties;
 
 public final class Util {
@@ -13,17 +22,11 @@ public final class Util {
     private static final String PASSWORD_KEY = "db.password";
     private static final String USERNAME_KEY = "db.username";
 
-    static {
-        loadProperties();
-    }
-
     private Util() {
     }
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection( PROPERTIES.getProperty(URL_KEY),
-                                            PROPERTIES.getProperty(USERNAME_KEY),
-                                            PROPERTIES.getProperty(PASSWORD_KEY));
+    static {
+        loadProperties();
     }
 
     public static void loadProperties() {
@@ -32,5 +35,29 @@ public final class Util {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(PROPERTIES.getProperty(URL_KEY),
+                PROPERTIES.getProperty(USERNAME_KEY),
+                PROPERTIES.getProperty(PASSWORD_KEY));
+    }
+
+    public static SessionFactory getSessionFactory() {
+        try {
+            return new Configuration()
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static <T> List<T> loadAllData(Class<T> type, Session session) {
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<T> criteria = builder.createQuery(type);
+        criteria.from(type);
+        return session.createQuery(criteria).getResultList();
     }
 }
